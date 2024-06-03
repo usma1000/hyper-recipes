2b36a585429d6d97e058b896d0b4efb407ae2d9b
usma1000 <usma1000@gmail.com>
2024-06-03 09:32:38 -0400

    scaffold ui

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 ## TODO
 
-- [ ] Make it deploy (vercel)
-- [ ] Scaffold basic UI with mock data
+- [x] Make it deploy (vercel)
+- [x] Scaffold basic UI with mock data
 - [ ] Set up db (vercel postgres)
 - [ ] Attach db to UI
 - [ ] Add auth (clerk)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/be838078-efd1-4855-80b3-6ee57ec952b9-567z33.webp",
+  "https://utfs.io/f/18a61f93-dcb4-4fdc-ba0d-581bb7c58f9b-9ff3qs.jpg",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello! App dev in progress.
+    <main className="">
+      <div className="flex flex-wrap gap-4 p-4">
+        {mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <Link href={`/recipe/${image.id}`}>
+              <img src={image.url} alt="" className="w-full rounded-md" />
+              <span className="mt-2 block font-semibold text-white">Title</span>
+              <p className="text-sm text-gray-400">Description</p>
+            </Link>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
