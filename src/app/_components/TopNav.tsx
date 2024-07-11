34cd2954b8ddc942588574b203889e31930d78c9
usma1000 <usma1000@gmail.com>
2024-07-11 07:17:37 -0400

    added modetoggle component

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -4,17 +4,19 @@ import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { UploadButton } from "~/utils/uploadthing";
+import { ModeToggle } from "./ModeToggle";
 
 export default function TopNav() {
   const router = useRouter();
   return (
-    <nav className="flex items-center justify-between bg-black p-4 text-xl font-semibold text-white">
+    <nav className="flex items-center justify-between p-4 text-xl font-semibold">
       <Link href="/" className="font-semibold">
         Hyper Recipes
       </Link>
 
       <div className="flex flex-row">
         <Link href="/new">Recipes</Link>
+        <ModeToggle />
         <SignedOut>
           <SignInButton />
         </SignedOut>
