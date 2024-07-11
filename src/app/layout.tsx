7a7e612961de853006341b25cfd4d528840f4c46
usma1000 <usma1000@gmail.com>
2024-07-11 06:58:27 -0400

    shad init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import "~/styles/globals.css";
 import "@uploadthing/react/styles.css";
+import { Inter as FontSans } from "next/font/google";
 
 import { ClerkProvider } from "@clerk/nextjs";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
@@ -7,6 +8,12 @@ import { extractRouterConfig } from "uploadthing/server";
 import { GeistSans } from "geist/font/sans";
 import TopNav from "./_components/TopNav";
 import { ourFileRouter } from "./api/uploadthing/core";
+import { cn } from "~/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export const metadata = {
   title: "Hyper Recipes",
@@ -25,7 +32,12 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" className={`${GeistSans.variable}`}>
         <NextSSRPlugin routerConfig={extractRouterConfig(ourFileRouter)} />
-        <body>
+        <body
+          className={cn(
+            "bg-background min-h-screen font-sans antialiased",
+            fontSans.variable,
+          )}
+        >
           <TopNav />
           <main className="p-8">
             {children}
