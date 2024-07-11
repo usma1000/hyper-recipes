7e335aced9842ab784aa668971f786d66959e5c9
usma1000 <usma1000@gmail.com>
2024-07-11 07:24:06 -0400

    comment out upload from nav

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -2,31 +2,31 @@
 
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { UploadButton } from "~/utils/uploadthing";
+// import { useRouter } from "next/navigation";
+// import { UploadButton } from "~/utils/uploadthing";
 import { ModeToggle } from "./ModeToggle";
 
 export default function TopNav() {
-  const router = useRouter();
+  // const router = useRouter();
   return (
-    <nav className="flex items-center justify-between p-4 text-xl font-semibold">
+    <nav className="flex items-center justify-between border-b border-slate-500 border-opacity-20 p-8 text-xl font-semibold">
       <Link href="/" className="font-semibold">
         Hyper Recipes
       </Link>
 
-      <div className="flex flex-row">
+      <div className="flex items-center gap-4">
         <Link href="/new">Recipes</Link>
         <ModeToggle />
         <SignedOut>
           <SignInButton />
         </SignedOut>
         <SignedIn>
-          <UploadButton
+          {/* <UploadButton
             endpoint="imageUploader"
             onClientUploadComplete={() => {
               router.refresh();
             }}
-          />
+          /> */}
           <UserButton />
         </SignedIn>
       </div>
