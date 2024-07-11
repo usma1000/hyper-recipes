bf97e2110412ed500e12d8f93d4f1c5f9476d160
usma1000 <usma1000@gmail.com>
2024-07-11 13:25:13 -0400

    added carousel component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ import {
   DialogFooter,
 } from "@/components/ui/dialog";
 import { Button, buttonVariants } from "@/components/ui/button";
-import { Star } from "lucide-react";
+import { Car, Star } from "lucide-react";
 import {
   Card,
   CardHeader,
@@ -22,6 +22,13 @@ import {
   CardDescription,
 } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export const dynamic = "force-dynamic";
 
@@ -29,79 +36,90 @@ async function Recipes() {
   const recipes = await getAllRecipes();
 
   return (
-    <div className="flex flex-wrap justify-center gap-4 p-4">
+    <div>
       {recipes.length === 0 && (
         <div className="w-full text-center text-2xl">No recipes yet</div>
       )}
-      {recipes.map((recipe) => {
-        return (
-          <div key={recipe.id} className="w-64">
-            <Dialog>
-              <DialogTrigger asChild>
-                <Card className="overflow-hidden transition-all hover:-translate-y-1 hover:cursor-pointer hover:shadow-lg">
-                  <div className="relative h-48">
-                    {recipe.heroImage?.url && (
-                      <Image
-                        src={recipe.heroImage.url}
-                        alt={recipe.heroImage.name}
-                        fill={true}
-                        style={{ objectFit: "cover" }}
-                      />
-                    )}
-                  </div>
-                  <CardHeader>
-                    <CardTitle>{recipe.name}</CardTitle>
-                    <CardDescription>{recipe.description}</CardDescription>
-                  </CardHeader>
-                </Card>
-              </DialogTrigger>
-              <DialogPortal>
-                <DialogOverlay />
-                <DialogContent>
-                  {recipe.heroImage?.url && (
-                    <DialogHeader className="relative h-64">
-                      <Image
-                        src={recipe.heroImage.url}
-                        alt={recipe.heroImage.name}
-                        fill={true}
-                        className="rounded-md shadow-sm"
-                        style={{ objectFit: "cover" }}
-                      />
-                    </DialogHeader>
-                  )}
-                  <DialogTitle>{recipe.name}</DialogTitle>
-                  <DialogDescription>{recipe.description}</DialogDescription>
-                  <div className="flex flex-row gap-2">
-                    <Badge variant="outline">Indian</Badge>
-                    <Badge variant="outline">Vegetarian</Badge>
-                  </div>
-                  <DialogFooter>
-                    <div className="flex w-full flex-row justify-between">
-                      <Link
-                        href={`/recipe/${recipe.id}`}
-                        className={buttonVariants({ variant: "default" })}
-                      >
-                        View Recipe
-                      </Link>
-                      <Button variant="outline" size="icon">
-                        <Star className="h-3 w-3" />
-                      </Button>
-                    </div>
-                  </DialogFooter>
-                </DialogContent>
-              </DialogPortal>
-            </Dialog>
-          </div>
-        );
-      })}
+      <Carousel className="w-full">
+        <CarouselContent className="py-4">
+          {recipes.map((recipe) => {
+            return (
+              <CarouselItem
+                key={recipe.id}
+                className="md:basis-1/2 lg:basis-1/2"
+              >
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Card className="h-full overflow-hidden transition-all hover:-translate-y-1 hover:cursor-pointer hover:shadow-md">
+                      <div className="relative h-48">
+                        {recipe.heroImage?.url && (
+                          <Image
+                            src={recipe.heroImage.url}
+                            alt={recipe.heroImage.name}
+                            fill={true}
+                            style={{ objectFit: "cover" }}
+                          />
+                        )}
+                      </div>
+                      <CardHeader>
+                        <CardTitle>{recipe.name}</CardTitle>
+                        <CardDescription>{recipe.description}</CardDescription>
+                      </CardHeader>
+                    </Card>
+                  </DialogTrigger>
+                  <DialogPortal>
+                    <DialogOverlay />
+                    <DialogContent>
+                      {recipe.heroImage?.url && (
+                        <DialogHeader className="relative h-64">
+                          <Image
+                            src={recipe.heroImage.url}
+                            alt={recipe.heroImage.name}
+                            fill={true}
+                            className="rounded-md shadow-sm"
+                            style={{ objectFit: "cover" }}
+                          />
+                        </DialogHeader>
+                      )}
+                      <DialogTitle>{recipe.name}</DialogTitle>
+                      <DialogDescription>
+                        {recipe.description}
+                      </DialogDescription>
+                      <div className="flex flex-row gap-2">
+                        <Badge variant="outline">Indian</Badge>
+                        <Badge variant="outline">Vegetarian</Badge>
+                      </div>
+                      <DialogFooter>
+                        <div className="flex w-full flex-row justify-between">
+                          <Link
+                            href={`/recipe/${recipe.id}`}
+                            className={buttonVariants({ variant: "default" })}
+                          >
+                            View Recipe
+                          </Link>
+                          <Button variant="outline" size="icon">
+                            <Star className="h-3 w-3" />
+                          </Button>
+                        </div>
+                      </DialogFooter>
+                    </DialogContent>
+                  </DialogPortal>
+                </Dialog>
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 }
 
 export default function HomePage() {
   return (
-    <>
-      <h2>All Recipes</h2>
+    <div className="mx-auto max-w-3xl">
+      <h1>Featured Recipes</h1>
       <Recipes />
       <h2>My favorites</h2>
       <p>Favorites go here...</p>
@@ -111,6 +129,6 @@ export default function HomePage() {
       <SignedIn>
         <div>Signed in: Button to create or edit recipes.</div>
       </SignedIn>
-    </>
+    </div>
   );
 }
