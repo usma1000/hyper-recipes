0af303e79a6d673f18714353e8bcfdfa01cf9af2
usma1000 <usma1000@gmail.com>
2024-07-18 16:22:51 -0400

    fixed footer styling

--- src/app/_components/Footer.tsx
+++ src/app/_components/Footer.tsx
@@ -1,11 +1,15 @@
+import { Zap } from "lucide-react";
+import Link from "next/link";
+
 export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <footer className="border-t border-slate-200 bg-white py-4 text-slate-950 shadow-sm dark:border-slate-800 dark:bg-slate-950 dark:text-slate-50">
-      <div className="mx-auto flex max-w-3xl items-center justify-between font-semibold">
-        <div className="flex align-middle text-xl font-semibold">
-          <span>Hyper Recipes</span>
-        </div>
+      <div className="container flex items-center justify-between font-semibold">
+        <Link href="/" className="text-md flex align-middle font-semibold">
+          <Zap size={16} />
+          Hyper Recipes
+        </Link>
         <div className="flex items-center gap-4">
           <span>© {currentYear} Hyper Recipes</span>
         </div>
